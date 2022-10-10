90d200ec25aad40c89ffc1507466c448d65de39e
asecino32 <115490366+asecino32@users.noreply.github.com>
2022-10-10 20:55:07 -0300

    Add files via upload

---
+++ Proyecto/SIPM/SIPMapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Algo(models.Model):
+ 
+    nombre_algo = models.CharField(max_length=100)
+    nombre_usuario = models.CharField(max_length=100)
+    fecha = models.DateTimeField(auto_now=True)
