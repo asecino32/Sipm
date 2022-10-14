6805e568a16135efc5c56615b79b00cab2c5870e
felipequiroz <115595952+felipequiroz@users.noreply.github.com>
2022-10-14 17:31:23 -0300

    Update forms.py

--- Proyecto/SIPM/SIPMapp/forms.py
+++ Proyecto/SIPM/SIPMapp/forms.py
@@ -11,7 +11,7 @@ class NewRegister(UserCreationForm):
     
     def __init__(self,*args,**kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['username'].label = 'Nombre de usuario'
+        self.fields['username'].label = 'Nombre de Usuario'
         self.fields['password1'].label = 'Contraseña'
         self.fields['password2'].label = 'Confirmar contraseña'
         for fieldname in ['username', 'password1', 'password2']:
